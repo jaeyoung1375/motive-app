949eb8d767016ad2591165f835ea426be54b9b66
jaeyoung <jaeyoung1375@naver.com>
2026-08-21 08:30:32 +0900

    env 수정

--- lib/core/env/env.dart
+++ lib/core/env/env.dart
@@ -6,21 +6,12 @@ class Env {
     defaultValue: 'http://localhost:9090',
   );
 
-  static const String oauthGoogleUrl = String.fromEnvironment(
-    'OAUTH_GOOGLE_URL',
-    defaultValue:
-        'http://localhost:9090/oauth2/authorization/google?platform=mobile',
-  );
+  static const String oauthGoogleUrl =
+      '$apiBaseUrl/oauth2/authorization/google?platform=mobile';
 
-  static const String oauthKakaoUrl = String.fromEnvironment(
-    'OAUTH_KAKAO_URL',
-    defaultValue:
-        'http://localhost:9090/oauth2/authorization/kakao?platform=mobile',
-  );
+  static const String oauthKakaoUrl =
+      '$apiBaseUrl/oauth2/authorization/kakao?platform=mobile';
 
-  static const String oauthGithubUrl = String.fromEnvironment(
-    'OAUTH_GITHUB_URL',
-    defaultValue:
-        'http://localhost:9090/oauth2/authorization/github?platform=mobile',
-  );
+  static const String oauthGithubUrl =
+      '$apiBaseUrl/oauth2/authorization/github?platform=mobile';
 }
